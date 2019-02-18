2089aa190765cba2789dc95f579e15e35756ba61
Jian Liu <onanying@126.com>
2019-02-18 15:37:27 +0800

    独立mix/pool

---
+++ src/DialInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Mix\Pool;
+
+/**
+ * Interface DialInterface
+ * @author LIUJIAN <coder.keda@gmail.com>
+ * @package Mix\Pool
+ */
+interface DialInterface
+{
+
+    /**
+     * 处理
+     * @return mixed
+     */
+    public function handle();
+
+}
